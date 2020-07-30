08a460a09afb43debd9753b7aa0a5e646397be99
Luke MacKenzie <mackenzieluke94@gmail.com>
2020-07-30 18:20:16 -0400

    small change

--- content.js
+++ content.js
@@ -6,5 +6,12 @@ chrome.storage.local.set({ pageTitle: getPageH1() });
 
 function getPageH1() {
   const h1s = Array.from(document.getElementsByTagName('h1'));
-  return h1s[0].innerText;
+  let h1 = 'No page title was found';
+  for (let i = 0; i < h1s.length; i++) {
+    if (h1s[i].innerText) {
+      h1 = h1s[i].innerText;
+      break;
+    }
+  }
+  return h1;
 }
